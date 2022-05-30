9391198a349ccd201be1816ffc45ab57c73e984d
Jiminy-Cricket23 <91724927+Jiminy-Cricket23@users.noreply.github.com>
2022-05-30 10:41:32 +0200

    Added input validation code

---
+++ app/src/main/java/com/varsitycollege/cardocity_app/InputValidation.java
@@ -0,0 +1,80 @@
+package com.varsitycollege.cardocity_app;
+
+import android.widget.Toast;
+
+public class InputValidation {
+    //Constructor method
+    public InputValidation()
+    {
+
+    }
+
+    //Method to validate a string is not null or empty
+    public boolean NotNullorEmpty(String sInput)
+    {
+        return !(sInput.isEmpty() || sInput.equals(null));
+    }
+    //Method to check if a string contains a number
+    public boolean ContainsNumber(String sInput)
+    {
+        boolean bFlag = false;
+        char[] arrInput = sInput.toCharArray();
+
+        for(int i = 0; i < arrInput.length; i++)
+        {
+            if(Character.isDigit(arrInput[i]))
+            {
+                bFlag = true;
+                break;
+            }
+        }
+        return bFlag;
+    }
+
+
+    // method to validate if it is a valid username
+    public boolean ValidateUsername(String sUsername)
+    {
+        boolean[] arrFlag = {true, true, true, true};
+        if (!NotNullorEmpty(sUsername))
+        {
+            ErrorMessage("Username is Empty!");
+            arrFlag[0] = false;
+        }
+        if(!ContainsNumber(sUsername))
+        {
+            ErrorMessage("Username does not contain a number!");
+            arrFlag[1] = false;
+        }
+        if(sUsername.length() >= 5)
+        {
+            ErrorMessage("Username is too short");
+            arrFlag[2] = false;
+        }
+        if(sUsername.length() <= 20)
+        {
+            ErrorMessage("Username is too long");
+            arrFlag[3] = false;
+        }
+        boolean bFlag = true;
+        for (int i = 0; i < arrFlag.length; i++)
+        {
+            if (!arrFlag[i])
+            {
+                bFlag = false;
+                break;
+            }
+        }
+
+        return bFlag;
+    }
+
+    // this method shows an error message with the string it is given
+    private void ErrorMessage(String sError)
+    {
+        Toast.makeText(null, sError, Toast.LENGTH_SHORT).show();
+    }
+
+    // TODO Validate password - 6 chars, contains a number, contains capital letter
+    // TODO Validate email
+}
