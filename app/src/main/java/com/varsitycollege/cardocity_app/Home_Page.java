4552c59eb29694e98890a448b03faba181d4419b
Dylan Ainsworth <dylan.ainsworth@gmail.com>
2022-05-31 17:20:17 +0200

    navbar testing

--- app/src/main/java/com/varsitycollege/cardocity_app/Home_Page.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Home_Page.java
@@ -1,14 +1,41 @@
 package com.varsitycollege.cardocity_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Home_Page extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout; //DylanA
+    private ActionBarDrawerToggle mToggle; //DylanA
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_10_collection_screen);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);//DylanA
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close); //DylanA
+
+        mDrawerLayout.addDrawerListener(mToggle);//DylanA
+        mToggle.syncState();//DylanA
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//DylanA
+    }
+//enable action bar tabbing DylanA
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {//DylanA
+
+        if(mToggle.onOptionsItemSelected(item)){//DylanA
+            return true;//DylanA
+        }
+
+        return super.onOptionsItemSelected(item);//DylanA
     }
 }
