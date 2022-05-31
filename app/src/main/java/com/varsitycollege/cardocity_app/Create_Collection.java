a1dab991893ecaddddd8e567ea0dbb7caea7006d
lab_services_student <jadejallahrs@gmail.com>
2022-05-31 23:50:43 +0200

    Create Collection

--- app/src/main/java/com/varsitycollege/cardocity_app/Create_Collection.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Create_Collection.java
@@ -1,29 +1,18 @@
 package com.varsitycollege.cardocity_app;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
 
-public class Home_Page extends AppCompatActivity {
-    Button CreateCollectionBtn;
+public class Create_Collection extends AppCompatActivity {
     private DrawerLayout mDrawerLayout; //DylanA
     private ActionBarDrawerToggle mToggle; //DylanA
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_10_collection_screen);
-CreateCollectionBtn = findViewById(R.id.HP_Create_Collection);
-        CreateCollectionBtn.setOnClickListener(view -> {
-            startActivity(new Intent(Home_Page.this,Create_Collection.class));
-        });
+        setContentView(R.layout.page_3_create_collection_screen);
         // enable ActionBar app icon to behave as action to toggle nav drawer
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//DylanA
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -34,8 +23,6 @@ CreateCollectionBtn = findViewById(R.id.HP_Create_Collection);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close); //DylanA
         mToggle.syncState();//DylanA
     }
-//enable action bar tabbing DylanA
-
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {//DylanA
