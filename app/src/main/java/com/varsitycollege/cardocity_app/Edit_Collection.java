83d867267d7b616dffef1b3b35cab9bb55f70cc3
lab_services_student <jadejallahrs@gmail.com>
2022-06-01 12:48:23 +0200

    update and delete Collection buttons

--- app/src/main/java/com/varsitycollege/cardocity_app/Edit_Collection.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Edit_Collection.java
@@ -3,12 +3,33 @@ package com.varsitycollege.cardocity_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Edit_Collection extends AppCompatActivity {
-
+Button updateCollectionBtn;
+Button deletedCollectionBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_collection);
+
+        updateCollectionBtn = findViewById(R.id.updateCollectionButton);
+        deletedCollectionBtn = findViewById(R.id.deleteCollectionButton);
+
+
+
+        updateCollectionBtn.setOnClickListener(view -> {
+
+            //Update Collection method
+            //push back to Collection screen when done
+
+        });
+
+        deletedCollectionBtn.setOnClickListener(view -> {
+            //Delete Collection Method
+            //Push back to Collection Screen when done
+
+
+        });
     }
 }
