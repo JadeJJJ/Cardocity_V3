737333b622c85168d2f6449e592000da97769bb8
Jiminy-Cricket23 <91724927+Jiminy-Cricket23@users.noreply.github.com>
2022-06-01 15:32:05 +0200

    - added button name for add item - assigned button

--- app/src/main/java/com/varsitycollege/cardocity_app/Cards_In_Collection.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Cards_In_Collection.java
@@ -9,12 +9,16 @@ import android.widget.Button;
 public class Cards_In_Collection extends AppCompatActivity {
 Button addItemBtn;
 Button editCollectionBtn;
+Button btnSelect;
+Button btnAddToDeck;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards_in_collection);
         addItemBtn=findViewById(R.id.addItem);
         editCollectionBtn = findViewById(R.id.editCollectionButton);
+        btnSelect = findViewById(R.id.btnSelectItem);
+        btnAddToDeck = findViewById(R.id.btnAddtoDeck);
 
 
 
