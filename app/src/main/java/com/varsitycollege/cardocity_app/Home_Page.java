7138870763063cf1bd42bd2b662cab82546260ab
Jiminy-Cricket23 <91724927+Jiminy-Cricket23@users.noreply.github.com>
2022-06-02 13:35:12 +0200

    - Added validation and add collection code - Added Getting the collection from the DB directly - Added StringOut Method to collections to better format it

--- app/src/main/java/com/varsitycollege/cardocity_app/Home_Page.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Home_Page.java
@@ -8,30 +8,71 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home_Page extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    Button CreateCollectionBtn;
-    Button SelectCollectionBTN;
+    private Button CreateCollectionBtn;
+    private Button SelectCollectionBTN;
     private DrawerLayout mDrawerLayout; //DylanA
     private ActionBarDrawerToggle mToggle; //DylanA
+    private ListView lstvCollections;
+    private ArrayAdapter<String> collAdapter;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference collRef = database.getReference("Collection");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_10_collection_screen);
-CreateCollectionBtn = findViewById(R.id.HP_Create_Collection);
+        CreateCollectionBtn = findViewById(R.id.HP_Create_Collection);
+        SelectCollectionBTN = findViewById(R.id.HP_Select_Collection);
+        lstvCollections = findViewById(R.id.lstvCollections);
+        List<String> collList;
+
+// ListView Show------------------------------------------------------------------------------------
+        collList = new ArrayList<>();
+
+        collRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                for (DataSnapshot pulledOrder : snapshot.getChildren()){
+                    Collection coll = pulledOrder.getValue(Collection.class);
+                    collList.add(coll.StringOut());
+                }
+
+                // create the adapter to display the items
+                collAdapter = new ArrayAdapter<String>(Home_Page.this, android.R.layout.simple_list_item_1, collList);
+                lstvCollections.setAdapter(collAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Home_Page.this, "Error Reading from Database", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+// Button Clicks------------------------------------------------------------------------------------
         CreateCollectionBtn.setOnClickListener(view -> {
             startActivity(new Intent(Home_Page.this,Create_Collection.class));
         });
-        SelectCollectionBTN = findViewById(R.id.HP_Select_Collection);
+
         SelectCollectionBTN.setOnClickListener(view ->{
             startActivity(new Intent(Home_Page.this,Cards_In_Collection.class));
-
-    });
+        });
 
 // NAV DRAWER---------------------------------------------------------------------------------------
         // enable ActionBar app icon to behave as action to toggle nav drawer
@@ -66,10 +107,6 @@ CreateCollectionBtn = findViewById(R.id.HP_Create_Collection);
                 break;
 
         }
-
-
-
-
         return true;
     }
 }
