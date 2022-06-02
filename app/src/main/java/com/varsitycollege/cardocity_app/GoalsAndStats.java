d6804494360559a99e8af88d40ecad8114b1fda4
Dylan Ainsworth <dylan.ainsworth@gmail.com>
2022-06-02 13:52:58 +0200

    Goals and Stays Layout + activity

---
+++ app/src/main/java/com/varsitycollege/cardocity_app/GoalsAndStats.java
@@ -0,0 +1,16 @@
+package com.varsitycollege.cardocity_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalsAndStats extends AppCompatActivity {
+Button btnUpdateChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards_in_collection);
+    }
+
+
+}
