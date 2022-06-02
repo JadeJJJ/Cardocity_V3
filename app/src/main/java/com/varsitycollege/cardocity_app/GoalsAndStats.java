9cbc0cdd47585dcdf0997062bcc13c3302c8308c
Dylan Ainsworth <dylan.ainsworth@gmail.com>
2022-06-02 14:08:35 +0200

    Nav Drawer for Goals and Stats

--- app/src/main/java/com/varsitycollege/cardocity_app/GoalsAndStats.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/GoalsAndStats.java
@@ -1,15 +1,35 @@
 package com.varsitycollege.cardocity_app;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GoalsAndStats extends AppCompatActivity {
-Button btnUpdateChart;
+    private Button UpdateChartBTN;
+    private DrawerLayout mDrawerLayout; //DylanA
+    private ActionBarDrawerToggle mToggle; //DylanA
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cards_in_collection);
+        setContentView(R.layout.activity_stats_and_goals);
+// Button Clicks------------------------------------------------------------------------------------
+        UpdateChartBTN = findViewById(R.id.btnUpdateChart);
+        //TODO: Update Pie Chart FUNCTION
+
+// NAV DRAWER---------------------------------------------------------------------------------------
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//DylanA
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);//DylanA EVERY PAGE NEEDS A DRAWERLAYOUT ID
+        mDrawerLayout.addDrawerListener(mToggle);//DylanA
+
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close); //DylanA
+        mToggle.syncState();//DylanA
+// -------------------------------------------------------------------------------------------------
     }
 
 
