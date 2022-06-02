c01fb89f6729afe2566e60cd1997f436e20b1422
Jiminy-Cricket23 <91724927+Jiminy-Cricket23@users.noreply.github.com>
2022-06-02 14:33:23 +0200

    - Added item display

--- app/src/main/java/com/varsitycollege/cardocity_app/Cards_In_Collection.java
+++ app/src/main/java/com/varsitycollege/cardocity_app/Cards_In_Collection.java
@@ -1,11 +1,24 @@
 package com.varsitycollege.cardocity_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Cards_In_Collection extends AppCompatActivity {
     private Button addItemBtn;
@@ -13,6 +26,8 @@ public class Cards_In_Collection extends AppCompatActivity {
     private Button btnSelect;
     private Button btnAddToDeck;
     private ListView lstvOrderHistory;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference itemRef = database.getReference("Item");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +39,28 @@ public class Cards_In_Collection extends AppCompatActivity {
 
 
         // TODO Add items from the database to the list in a collection
+// Adding to List View------------------------------------------------------------------------------
+        List<String> itemList = new ArrayList<>();
+        ListView lstvItems = findViewById(R.id.lstvCards);
+        String userid = MainActivity.UserID;
+        itemRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                for (DataSnapshot pulledOrder : snapshot.getChildren()){
+                    Item item = pulledOrder.getValue(Item.class);
+                   // if (Objects.equals(item.getUserID(), userid))
+                        itemList.add(item.toString());
+                }
 
+                ArrayAdapter<String> collAdapter = new ArrayAdapter<String>(Cards_In_Collection.this, android.R.layout.simple_list_item_1, itemList);
+                lstvItems.setAdapter(collAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Cards_In_Collection.this, "Error Reading from Database", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         editCollectionBtn.setOnClickListener(view -> {
             startActivity(new Intent(Cards_In_Collection.this,Edit_Collection.class));
